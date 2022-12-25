40e15c9c67b888483adc782b23daa8b2a06c2ec3
Andrew285 <andriyk116@gmail.com>
2022-12-25 18:44:06 +0200

    Add project files.

---
+++ WebShopProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebShopProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
