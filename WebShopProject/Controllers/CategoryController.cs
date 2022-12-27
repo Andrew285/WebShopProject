2f8e13d3836a07772852e4ce8d5ef87985ce7b16
Andrew285 <andriyk116@gmail.com>
2022-12-27 17:26:09 +0200

    Category Controller and View

---
+++ WebShopProject/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebShopProject.Data;
+using WebShopProject.Models;
+
+namespace WebShopProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = db.Categories;
+            return View(categoryList);
+        }
+    }
+}
