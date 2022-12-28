520c5961bdf601b97be0618f437c32c474073f52
Andrew285 <andriyk116@gmail.com>
2022-12-28 17:39:47 +0200

    add Bootstrap / Category View

--- WebShopProject/Startup.cs
+++ WebShopProject/Startup.cs
@@ -26,6 +26,7 @@ namespace WebShopProject
                 ));
 
             services.AddControllersWithViews();
+            //services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
