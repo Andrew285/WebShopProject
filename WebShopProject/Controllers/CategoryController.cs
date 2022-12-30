862daaf1822614b657af06435af89bee3b1f40ea
Andrew285 <andriyk116@gmail.com>
2022-12-30 22:13:04 +0200

    basic validation data / get and post method

--- WebShopProject/Controllers/CategoryController.cs
+++ WebShopProject/Controllers/CategoryController.cs
@@ -19,5 +19,29 @@ namespace WebShopProject.Controllers
             IEnumerable<Category> categoryList = db.Categories;
             return View(categoryList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
     }
 }
--- WebShopProject/Models/Category.cs
+++ WebShopProject/Models/Category.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebShopProject.Models
 {
@@ -10,8 +8,12 @@ namespace WebShopProject.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "The Display Order must be between 1 and 100")]
         public int DisplayOrder { get; set; }
         public DateTime createdDateTime { get; set; } = DateTime.Now;
     }
