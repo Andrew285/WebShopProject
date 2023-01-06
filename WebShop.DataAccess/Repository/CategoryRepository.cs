abeaf6759f34b8c89c4c899a5f99b2deb5cd0ee7
Andrew285 <andriyk116@gmail.com>
2023-01-06 11:26:08 +0200

    Implementing Repository Pattern

---
+++ WebShop.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.DataAccess.Repository.IRepository;
+using WebShop.Models;
+
+namespace WebShop.DataAccess.Repository
+{
+    class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext db;
+
+        public CategoryRepository(ApplicationDbContext _db): base(_db)
+        {
+            db = _db;
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            db.Categories.Update(obj);
+        }
+    }
+}
---
+++ WebShop.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace WebShop.DataAccess.Repository.IRepository
+{
+    interface IRepository<T> where T: class
+    {
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+    }
+}
